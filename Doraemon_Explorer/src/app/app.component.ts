ca9e4df5ea2b7d0db9f98368360d6c3131f0ed5c
dishasachdeva <127543878+DishaSachdeva-02@users.noreply.github.com>
2024-03-25 20:43:29 +0530

    update

--- Doraemon_Explorer/src/app/app.component.ts
+++ Doraemon_Explorer/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import { MessagesComponent } from './messages/messages.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule,SearchComponent],
+  imports: [RouterOutlet,RouterModule,SearchComponent,MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Doraemon_Explorer';
+  title = 'Doraemon Explorer';
 }
